01c11ac77dc827f5ebc00e4b0c7b4e14fffc04c9
Elijah Miller <elijah.miller@gmail.com>
2026-05-24 18:05:01 -0400

    Update to v0.0.1

--- jqrutils.rb
+++ jqrutils.rb
@@ -2,7 +2,7 @@ class Jqrutils < Formula
   desc "Small Unix command-line tools"
   homepage "https://github.com/jqr/jqrutils"
   url "https://github.com/jqr/jqrutils/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "a7a97485d704ef09986ae6e563d69d878e0a6f31bd4cc08a00da5b9f7334dbd3"
   license "MIT"
 
   depends_on "rust" => :build
